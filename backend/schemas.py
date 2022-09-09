2cf14310537e8d24d56049a3cb6094be4afdab7f
Matt Turinski <matt.turinski@gmail.com>
2022-09-09 08:58:11 -0400

    Adding basic user creation, and oauth tokens.

---
+++ backend/schemas.py
@@ -0,0 +1,18 @@
+import datetime as _dt
+import pydantic as _pydantic
+
+class _UserBase(_pydantic.BaseModel):
+    username: str
+
+class UserCreate(_UserBase):
+    password: str
+
+    class Config:
+        orm_mode = True
+
+class User(_UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
